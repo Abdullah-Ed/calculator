ccf59a46652f7dcb40573ca751e6ae9e5c305c20
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-04-14 03:09:27 +0200

    Add operate function to perform basic math operations on 2 numbers

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function divide(a,b) {
 } 
 
 
+const num1 = 3;
+const operator = '+';
+const num2 = 5;
+
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+    default:
+      return 'Invalid operator';
+  }
+}
+
+
